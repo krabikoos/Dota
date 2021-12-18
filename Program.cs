c9a153d3f32058ff75db8d240c986dece41d138c
dark <81390856+Darkering@users.noreply.github.com>
2021-12-18 12:55:15 +0300

    CreatFile

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Dota
@@ -11,14 +12,31 @@ namespace Dota
     {
         static void Main(string[] args)
         {
-            var Application = new Excel.Application();
-            Application.SheetsInNewWorkbook = 3;
-            Excel.Workbook WorkBook = Application.Workbooks.Add();
-            Excel.Worksheet Sheet = (Excel.Worksheet)Application.Worksheets.Item[1];
+            //Создаёт файл Excel просто с названиями листов
+            if (!File.Exists("C:\\Users\\Zalman\\Desktop\\Код\\C#\\Dota\\Dota.xlsx"))
+            {
+                var Application = new Excel.Application();
+                Application.SheetsInNewWorkbook = 4;
+                Excel.Workbook WorkBook = Application.Workbooks.Add();
 
-            WorkBook.SaveAs("C:\\Users\\Zalman\\Desktop\\Код\\C#\\Dota.xls");
-            WorkBook.Close();
-            Application.Quit();
+                Excel.Worksheet Workers;
+                Excel.Worksheet People;
+                Excel.Worksheet Items;
+                Excel.Worksheet BuyList;
+
+                Workers = (Excel.Worksheet)Application.Worksheets.Item[1];
+                Workers.Name = "Список Сатрудников";
+                People = (Excel.Worksheet)Application.Worksheets.Item[2];
+                People.Name = "Список клиентов";
+                Items = (Excel.Worksheet)Application.Worksheets.Item[3];
+                Items.Name = "Список товаров";
+                BuyList = (Excel.Worksheet)Application.Worksheets.Item[4
+                    ];
+                BuyList.Name = "Список Заказов";
+                WorkBook.SaveAs("C:\\Users\\Zalman\\Desktop\\Код\\C#\\Dota\\Dota.xlsx");
+                Application.Quit();
+            }
+            
         }
     }
 }
