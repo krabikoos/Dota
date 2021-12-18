ed99fd66a0e677a95cf1f4694a3033fea891f596
dark <81390856+Darkering@users.noreply.github.com>
2021-12-18 11:58:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Dota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Application = new Excel.Application();
+            Application.SheetsInNewWorkbook = 3;
+            Excel.Workbook WorkBook = Application.Workbooks.Add();
+            Excel.Worksheet Sheet = (Excel.Worksheet)Application.Worksheets.Item[1];
+
+            WorkBook.SaveAs("C:\\Users\\Zalman\\Desktop\\Код\\C#\\Dota.xls");
+            WorkBook.Close();
+            Application.Quit();
+        }
+    }
+}
